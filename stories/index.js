35e19bb348cd5d50f1763a650dbaa7a21ca6fc9a
Wesley Corner <wescorner@gmail.com>
2022-08-10 19:39:13 -0400

    add Header and Empty components

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,9 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointments/index";
+import Header from "components/Appointments/Header";
+import Empty from "components/Appointments/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -108,3 +111,12 @@ storiesOf("InterviewerList", module)
   .add("Clickable", () => (
     <InterviewerList interviewers={interviewers} onChange={action("setInterviewer")} />
   ));
+
+storiesOf("Appointments", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
