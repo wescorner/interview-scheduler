3e5f127c4f59214b7237a69c12e81c21c38798f1
Wesley Corner <wescorner@gmail.com>
2022-08-12 21:27:24 -0400

    add getInterviewersForDay function and test suite for getting array of interviewer objects for a given day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,6 @@
 export function getAppointmentsForDay(state, day) {
   if (!state || !day || state.days.length === 0 || state.appointments.length === 0) return [];
-  const dayObj = state.days.filter((x) => x.name === day)[0];
+  const dayObj = state.days.find((x) => x.name === day);
   if (!dayObj) return [];
   return Object.values(state.appointments).filter((appointment) =>
     dayObj.appointments.includes(appointment.id)
@@ -10,4 +10,13 @@ export function getAppointmentsForDay(state, day) {
 export function getInterview(state, interview) {
   if (!interview) return null;
   return { student: interview.student, interviewer: state.interviewers[interview.interviewer] };
+}
+
+export function getInterviewersForDay(state, day) {
+  if (!state || !day || state.days.length === 0 || !state.interviewers) return [];
+  const dayObj = state.days.find((x) => x.name === day);
+  if (!dayObj || !dayObj.interviewers) return [];
+  return dayObj.interviewers.map((id) => {
+    return state.interviewers[id];
+  });
 }
