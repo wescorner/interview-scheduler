f51e2fa0be42bcff4c56c1c39bc37a1c3235da1c
Wesley Corner <wescorner@gmail.com>
2022-08-16 18:41:29 -0400

    spots remaining now dynamically update

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -26,6 +26,23 @@ export default function useApplicationData() {
 
   const setDay = (day) => setState({ ...state, day });
 
+  const updateSpots = function (state, appointments) {
+    let spots = 0;
+    const dayObj = state.days.find((day) => day.name === state.day);
+
+    for (const id of dayObj.appointments) {
+      const appointment = appointments[id];
+      if (!appointment.interview) {
+        spots++;
+      }
+    }
+
+    const newDay = { ...dayObj, spots };
+    const newDays = state.days.map((day) => (day.name === state.day ? newDay : day));
+
+    return newDays;
+  };
+
   const bookInterview = function (id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -36,9 +53,10 @@ export default function useApplicationData() {
       [id]: appointment,
     };
 
-    return axios
-      .put(`http://localhost:8000/api/appointments/${id}`, { interview })
-      .then(() => setState({ ...state, appointments }));
+    return axios.put(`http://localhost:8000/api/appointments/${id}`, { interview }).then(() => {
+      const days = updateSpots(state, appointments);
+      setState({ ...state, appointments, days });
+    });
   };
 
   const cancelInterview = function (id) {
@@ -50,9 +68,10 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment,
     };
-    return axios
-      .delete(`http://localhost:8000/api/appointments/${id}`)
-      .then(() => setState({ ...state, appointments }));
+    return axios.delete(`http://localhost:8000/api/appointments/${id}`).then(() => {
+      const days = updateSpots(state, appointments);
+      setState({ ...state, appointments, days });
+    });
   };
   return { state, setDay, bookInterview, cancelInterview };
 }
